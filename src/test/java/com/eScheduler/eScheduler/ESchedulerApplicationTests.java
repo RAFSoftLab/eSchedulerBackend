b9e6723c5c552eae7ea0eb78ccef63cf48f9a0e8
Ivan Cicka <ivan.cicka.ic@gmail.com>
2024-11-15 13:41:51 +0100

    Hello world

---
+++ src/test/java/com/eScheduler/eScheduler/ESchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eScheduler.eScheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ESchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
