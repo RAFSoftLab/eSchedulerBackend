7dc6bc6c4c0ea808d5b622b96af3583bc505c01e
Ivan Cicka <ivan.cicka.ic@gmail.com>
2025-01-29 13:25:28 +0100

    allow origins

--- src/main/java/com/eScheduler/WebConfig.java
+++ src/main/java/com/eScheduler/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         // Dozvoli CORS za sve rute sa svih domena
         registry.addMapping("/api/**")  //CORS za sve endpoint-e
-                .allowedOrigins("http://localhost:2526")  // samo Moja Angular aplikacija
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")  // Dozvoli sve zaglavlja
                 .allowCredentials(true);
